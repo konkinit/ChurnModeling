54c4b875e820d8de03d75768b04b8cac8e1a9b1e
kidrissa <ikonkobo.idrissa@gmail.com>
2022-06-22 21:29:35 +0000

    commit

--- src/data/import_data.py
+++ src/data/import_data.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 # enabling conexion to s3 storage for data retrieving
 
-def import_from_S3(key_id, access_key, token):
+def import_from_S3(bucket, path, key_id, access_key, token):
     fs = s3fs.S3FileSystem(client_kwargs={'endpoint_url':'https://'+'minio.lab.sspcloud.fr'}, key=key_id, secret=access_key, token=token)
-    return pd.read_sas(fs.open("ikonkobo/commsdata/commsdata.sas7bdat"), format='sas7bdat')
+    return pd.read_sas(fs.open(bucket+"/"+path+"/commsdata.sas7bdat"), format='sas7bdat')
