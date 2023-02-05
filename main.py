5f8362b03a51b9be079acd17e13e3ba0eb4e9cbf
kidrissa <ikonkobo.idrissa@gmail.com>
2023-02-05 13:15:31 +0100

    designing sparse matrix

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 """Main script of the project."""
-import yaml
 import os
 from src.data.import_data import import_from_local
 from src.data.train_valid_split import train_valid_split
 from src.features.build_features import MetaDataManagement, DataManagement
-from src.models.train_evaluate import evaluate_rdmf
+from src.models.random_forest import evaluate_rdmf
 
 os.chdir("/home/ikonkobo/Desktop/Self_Learning/telco_churn/")
 
@@ -12,15 +11,16 @@ if __name__ == "__main__":
     # data
     data = import_from_local(".")
 
-    # metadata variable management pipeline
+    ## metadata variable management pipeline
     MetaDataManagement(data).metadata_management_pipeline()
 
-    ## splitting
+    ### splitting
     X_train, X_valid, y_train, y_valid = train_valid_split(data)
-
+    
     ## data variable management pipeline
     DataManagement(X_train).data_management_pipeline()
     DataManagement(X_valid).data_management_pipeline()
+
 
     # models pipelines
     ## random forest model
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,6 +1,6 @@
 from numpy import max, log
 from pandas import (
-    DataFrame, concat, get_dummies)
+    DataFrame, qcut)
 from typing import List
 from sklearn.preprocessing import OneHotEncoder
 
@@ -32,19 +32,20 @@ class DataProcessing:
         for var in list_vars_object:
             self.data[var] = self.data[var].apply(lambda x : x.decode("utf-8"))
 
-
+    
     def lower_limit(self) -> None:
         """
         some variables intuitively cannot have negative values, 
         replace negative values with 0 
         """
-        list_ = ["tot_mb_data_roam_curr", "seconds_of_data_norm", "lifetime_value", 
-                "bill_data_usg_m03", "bill_data_usg_m06", "voice_tot_bill_mou_curr",
-                "tot_mb_data_curr", "mb_data_usg_roamm01", "mb_data_usg_roamm02", 
+        list_ = ["tot_mb_data_roam_curr", "seconds_of_data_norm", 
+                "lifetime_value", "bill_data_usg_m03", "bill_data_usg_m06", 
+                "voice_tot_bill_mou_curr", "tot_mb_data_curr", 
+                "mb_data_usg_roamm01", "mb_data_usg_roamm02", 
                 "mb_data_usg_roamm03", "mb_data_usg_m01", "mb_data_usg_m02", 
                 "mb_data_usg_m03", "calls_total", "calls_in_pk", "calls_out_pk", 
-                "calls_in_offpk", "calls_out_offpk", "mb_data_ndist_mo6m", "data_device_age",
-                "mou_onnet_pct_MOM", "mou_total_pct_MOM"]
+                "calls_in_offpk", "calls_out_offpk", "mb_data_ndist_mo6m", 
+                "data_device_age", "mou_onnet_pct_MOM", "mou_total_pct_MOM"]
         for var in list_:
             self.data[var] = self.data[var].apply(lambda x : max(x, 0))
 
@@ -61,59 +62,22 @@ class DataProcessing:
             self.data.drop(columns=[f"MB_Data_Usg_M0{str(i)}"], inplace=True)
 
 
-    def missing_var(self) -> List:
-        """
-        retrieving the list of variables having missing values
-        """
-        df_missing = (self.data
-                        .isnull()
-                        .sum()
-                        .to_frame()
-                        .reset_index()
-                    )   
-        df_missing.columns = ["variable", "missing_nb"]
-        df_missing = df_missing[df_missing["missing_nb"] > 0].reset_index(drop=True)
-        df_missing = (df_missing
-                        .sort_values('missing_nb', ascending=False)
-                        .reset_index(drop=True)
-                    )
-        return list(df_missing["variable"])
 
-    
-    def add_missing_indicator(self) -> None:
-        """
-        create missing indicator for features with missingness
-        """
-        list_missing_var = self.missing_var()
-        df_missing_indicator = (self.data[list_missing_var]
-                                    .isnull()
-                                    .astype(int)
-                                    .add_suffix("_MI")
-                                )
-        self.data = concat([self.data, df_missing_indicator], axis=1)
-    
+    def interval_vars_binning(self)-> None:
+        l_float_vars = self.data.select_dtypes(exclude='object').columns.to_list()
+        l_float_vars.remove("churn")
+        for num_var in l_float_vars:
+            self.data[f"{num_var}_bin"] = qcut(
+                                        self.data[num_var],
+                                        4,
+                                        duplicates='drop').astype(str)
+        
+        self.data.drop(l_float_vars, axis=1, inplace=True)
 
-    def imputation(self):
-        """
-        impute missing values with right method
-        """
-        list_missing_var = self.missing_var()
-        assert (
-            len(list_missing_var) > 0
-        ), "Columns with missing values"
-        for var in list_missing_var:
-            if len(self.data[var].unique()) > 50:
-                """
-                condition that a variable is continious
-                """
-                self.data[var].fillna(self.data[var].mean(), inplace=True)
-            else :
-                self.data[var].fillna(
-                    self.data[var].value_counts(ascending=False
-                    ).to_frame().reset_index().iloc[0, 0], inplace=True)
-    
+
     def list_object_vars(self) -> List:
         return self.data.select_dtypes(include='object').columns.to_list()
+
 
     def onehot_encoding(self) -> None:
         """
@@ -121,13 +85,17 @@ class DataProcessing:
         to let in the dataframe for text_mining
         """
         l_object_vars = self.list_object_vars()
-        enc = OneHotEncoder(handle_unknown='ignore')
-        df_ = DataFrame(data=enc.fit_transform(self.data[l_object_vars]).toarray(), 
-            columns=list(enc.get_feature_names_out())).astype(int)
+        l_object_vars.remove("verbatims")
+        enc = OneHotEncoder(handle_unknown='ignore', dtype=int)
+        df_ = DataFrame(
+            data=enc.fit_transform(self.data[l_object_vars].astype(str)).toarray(), 
+            columns=list(enc.get_feature_names_out()), 
+            index=self.data.index)
+
+        self.data[df_.columns.to_list()] = df_.iloc[:, :]
         self.data.drop(l_object_vars, axis=1, inplace=True)
-        self.data = concat([self.data, df_], 
-                            axis=1)
-        assert (len(self.list_object_vars()) == 0), "No character columns in data"
+
+        assert (len(self.list_object_vars()) == 1), "Other object vars than verbatims"
 
     
     def text_mining(self) -> None:
@@ -142,20 +110,22 @@ class MetaDataManagement(DataProcessing):
     def __init__(self, raw_data: DataFrame) -> None:
         super().__init__(raw_data)
 
+
     def metadata_management_pipeline(self):
         self.useless_feature()
         self.decode_char()
         self.lower_limit()
         self.log_transform()
-        self.add_missing_indicator()
+        self.interval_vars_binning()
+        self.onehot_encoding()
 
 
 class DataManagement(DataProcessing):
     def __init__(self, raw_data: DataFrame) -> None:
         super().__init__(raw_data)
 
+
     def data_management_pipeline(self):
-        self.imputation()
         self.text_mining()
-        self.onehot_encoding()
+        
         
