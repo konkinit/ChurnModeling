704834bf862e779474ae60e50bbf29c77a16209b
Idrissa KONKOBO <93009177+konkinit@users.noreply.github.com>
2023-02-14 00:32:01 +0100

    Dev (#5)
    * data processing
    
    * designing sparse matrix
    
    * trying streamlit
    
    * setting streamlit app
    
    * test & build
    
    * setting ci/cd workflows
    
    * ci/cd
    
    * setting up workflows
    
    * update README
    
    * streamlit app & ci/cd fix
    
    * test fix
    
    * metadata tables
    
    * metadat report
    
    * improving app
    
    * Update app.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,11 +6,12 @@ if os.getcwd() not in sys.path:
 
 def tests_file_present():
     """
-    test the presence of the data file
+    test the presence of the data/inputs file
     """
     required_files = []
     with open("required_files.txt", "r") as f:
         required_files.extend(line.strip() for line in f)
     print(required_files)
     for file in required_files:
-        print(os.path.isfile(os.path.join(file)))# f"{file} is not present"
+        assert os.path.isfile(os.path.join(file)), f"{file} is not present"
+
